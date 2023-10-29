e9e929df61cfc3a94f6b8801c4b0a1e456a09b3b
Naninidemo <trumpet.2100@gmail.com>
2023-10-29 23:41:50 +0900

    リレーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use App\Http\Requests\PostRequest;
+use App\Models\Category;
 
 
 class PostController extends Controller
@@ -20,10 +21,7 @@ class PostController extends Controller
     	return view('posts/show')->with(['post' => $post]);
     }
     
-    public function create()
-    {
-    	return view('posts.create');
-    }
+
     
     public function store(Post $post, PostRequest $request)
 	{
@@ -49,5 +47,10 @@ class PostController extends Controller
 	{
 		$post->delete();
 		return redirect('/');
+	}
+	
+	public function create(Category $category)
+	{
+    return view('posts.create')->with(['categories' => $category->get()]);
 	}
 }
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -16,11 +16,6 @@
                     <a href="/posts/{{$post->id}}">{{$post->title}}</a>
                     </h2>
                 <p class="body">{{$post->body}}</p>
-                <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
-                </form>
                 <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
             </div>
         @endforeach
@@ -28,16 +23,5 @@
     <div class='paginate'>
         {{ $posts->links() }}
     </div>
-<script>
-    function deletePost(id) {
-        'use strict'
-        
-        if (confirm('削除すると復元できません\n本当に削除しますか？'))　{
-            document.getElementById(`form_${id}`).submit();
-        }
-    }
-</script>
-
-
 </body>
 </html>
